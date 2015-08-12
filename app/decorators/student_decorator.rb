ef265129028ca4b05846a0e257c615a416c9573d
Nataly Tkachuk <techgirlua@gmail.com>
2015-08-12 18:00:34 +0300

    Add decorators for subject_item and teacher

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,12 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+  	avg = SubjectItemNote.where("subject_item_id = ?", subject_item.id).average(:value)
+  	if avg.nil?
+  		return "0.00"
+  	else	
+  		return "%.2f" % avg
+  	end
+
   end
 end
