e35dac46f5bc26ac6de529db4d94418e4f0416ba
Nataly Tkachuk <techgirlua@gmail.com>
2015-08-12 13:49:03 +0300

    Add has_many associations to student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,9 @@
 class Student < ActiveRecord::Base
   has_many :participations, dependent: :destroy
-  has_many :subject_item_notes, dependent: :destroy
+  has_many :subject_items, through: :participations
+  has_many :subject_item_notes, through: :subject_items
+
+  accepts_nested_attributes_for :subject_items
 
   validates :first_name, :last_name, presence: true
 end
